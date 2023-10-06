ae11c35353786d471e4b1b52caffc007e57e5c20
Natsume <natsume.al7@gmail.com>
2023-10-05 22:51:48 -0700

    Add solution for findEvenIndex

---
+++ 41-findEvenIndex.js
@@ -0,0 +1,18 @@
+//You are going to be given an array of integers.
+//Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N.
+//If there is no index that would make this happen, return -1.
+
+//i.g. {1,2,3,4,3,2,1} returns 3 because (1 + 2 + 3) === (3 + 2 + 1);
+
+function findEvenIndex(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let leftSum = arr
+      .slice(0, i)
+      .reduce((total, current) => total + current, 0);
+    let rightSum = arr
+      .slice(i + 1)
+      .reduce((total, current) => total + current, 0);
+    if (leftSum === rightSum) return i;
+  }
+  return -1;
+}
