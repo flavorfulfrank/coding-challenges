2cd9d08e8169e23772bf3f2ac9b35a5bd23bd6fd
Natsume <natsume.al7@gmail.com>
2023-12-20 21:26:35 -0800

    Add solution for noEndingZeros

---
+++ 111-noEndingZeros.js
@@ -0,0 +1,14 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+
+function noTrailingZeros(n) {
+  return +n.toString().replace(/0+$/, "");
+}
