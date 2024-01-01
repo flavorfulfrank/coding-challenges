c6549f193084a884d46678edd7e44010ebe091df
Natsume <natsume.al7@gmail.com>
2024-01-01 11:45:32 -0800

    Add solution to isAnagram using Map

--- 22-isAnagram.js
+++ 22-isAnagram.js
@@ -1,5 +1,19 @@
 //Take two strings
 //Return true if t is an anagram of s
-function isAnagram(s, t) {
-  return s.split('').sort().join('') === t.split('').sort().join('');
-}
+// function isAnagram(s, t) {
+//   return s.split('').sort().join('') === t.split('').sort().join('');
+// }
+
+var isAnagram = function(s, t) {
+  if (s.length !== t.length) return false;
+  const mapS = new Map();
+  const mapT = new Map();
+  for (let i = 0; i < s.length; i++) {
+      mapS.set(s[i], mapS.get(s[i]) + 1 || 1);
+      mapT.set(t[i], mapT.get(t[i]) + 1 || 1);
+  }
+  for (const [letter, count] of mapS.entries()) {
+      if (count !== mapT.get(letter)) return false;
+  }
+  return true;
+};
